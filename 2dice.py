72a1a9d88b339f7de1040aef1fa25583b2c45601
Nathan <shep.nathan.d@gmail.com>
2019-11-11 15:38:51 -0500

    increased resolution/added error handling

--- 2dice.py
+++ 2dice.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import sys
 
 # Open an Image
 def open_image(path):
@@ -61,82 +62,102 @@ def convert_dice(image):
 	width, height = image.size
 
 	# Create new Image and a Pixel Map
-	new = create_image(width, height)
+	new = create_image(width*7, height*7)
 	pixels = new.load()
+
+	i = 0
+	j = 0
+	maxSaturation = 0
+	count = 0
+
+	while i < height:
+		while j < width:
+			pixel = get_pixel(image,i,j)
+			if pixel[2] > maxSaturation:
+				maxSaturation = pixel[2]
+			count += 1
+			j+=1
+		i+=1
 
 	i = 0
 	j = 0
 
 	# Transform to dice
-	while j < height-7:
-		while i < width-7:
+	while j < height:
+		while i < width:
 			# Get saturation
 			saturation = 0
-			for k in range(7):
-				for l in range(7):
-					pixel = get_pixel(image, i+l, j+k)
-					saturation += pixel[2]
-					saturation = saturation/14
+			pixel = get_pixel(image, i, j)
+			saturation += pixel[2]
 
 			for k in range(7):
 				for l in range(7):
-					pixels[i+l, j+k] = (255,255,255)
+					pixels[(i*7)+l, (j*7)+k] = (255,255,255)
 
 			# Transform to dice
-			if saturation > 15.83:
-				pixels[i+3, j+3] = (0,0,0)
-			elif saturation > 12.67:
-				pixels[i+1, j+2] = (0,0,0)
-				pixels[i+5, j+4] = (0,0,0)
-			elif saturation > 9.5:
-				pixels[i+1, j+1] = (0,0,0)
-				pixels[i+3, j+3] = (0,0,0)
-				pixels[i+5, j+5] = (0,0,0)
-			elif saturation > 6.33:
-				pixels[i+2, j+2] = (0,0,0)
-				pixels[i+2, j+4] = (0,0,0)
-				pixels[i+4, j+2] = (0,0,0)
-				pixels[i+4, j+4] = (0,0,0)
-			elif saturation > 3.17:
-				pixels[i+1, j+1] = (0,0,0)
-				pixels[i+5, j+1] = (0,0,0)
-				pixels[i+3, j+3] = (0,0,0)
-				pixels[i+1, j+5] = (0,0,0)
-				pixels[i+5, j+5] = (0,0,0)
+			if saturation > maxSaturation*(5/6):
+				pixels[(i*7)+3, (j*7)+3] = (0,0,0)
+			elif saturation > maxSaturation*(2/3):
+				pixels[(i*7)+1, (j*7)+2] = (0,0,0)
+				pixels[(i*7)+5, (j*7)+4] = (0,0,0)
+			elif saturation > maxSaturation*(1/2):
+				pixels[(i*7)+1, (j*7)+1] = (0,0,0)
+				pixels[(i*7)+3, (j*7)+3] = (0,0,0)
+				pixels[(i*7)+5, (j*7)+5] = (0,0,0)
+			elif saturation > maxSaturation*(1/3):
+				pixels[(i*7)+2, (j*7)+2] = (0,0,0)
+				pixels[(i*7)+2, (j*7)+4] = (0,0,0)
+				pixels[(i*7)+4, (j*7)+2] = (0,0,0)
+				pixels[(i*7)+4, (j*7)+4] = (0,0,0)
+			elif saturation > maxSaturation*(1/6):
+				pixels[(i*7)+1, (j*7)+1] = (0,0,0)
+				pixels[(i*7)+5, (j*7)+1] = (0,0,0)
+				pixels[(i*7)+3, (j*7)+3] = (0,0,0)
+				pixels[(i*7)+1, (j*7)+5] = (0,0,0)
+				pixels[(i*7)+5, (j*7)+5] = (0,0,0)
 			else:
-				pixels[i+2, j+1] = (0,0,0)
-				pixels[i+4, j+1] = (0,0,0)
-				pixels[i+2, j+3] = (0,0,0)
-				pixels[i+4, j+3] = (0,0,0)
-				pixels[i+2, j+5] = (0,0,0)
-				pixels[i+4, j+5] = (0,0,0)
-			i+=7
+				pixels[(i*7)+2, (j*7)+1] = (0,0,0)
+				pixels[(i*7)+4, (j*7)+1] = (0,0,0)
+				pixels[(i*7)+2, (j*7)+3] = (0,0,0)
+				pixels[(i*7)+4, (j*7)+3] = (0,0,0)
+				pixels[(i*7)+2, (j*7)+5] = (0,0,0)
+				pixels[(i*7)+4, (j*7)+5] = (0,0,0)
+			i+=1
 		i=0
-		j+=7
+		j+=1
 
 	# Return new image
 	return new
 
 # Main
 if __name__ == "__main__":
-	# Load Image (JPEG/JPG needs libjpeg to load)
-	original = open_image('/home/nathan/Pictures/img_001.png')
+	try:
+		# process input
+		input = sys.argv[1].split("/")
+		file = input[-1]
+		path = "/"
+		for i in range(len(input)-1):
+			path += input[i] + "/"
 
-	# Convert to Grayscale and save
-	new = convert_grayscale(original)
-	save_image(new, '/home/nathan/Pictures/gray.png')
+		# Load Image (JPEG/JPG needs libjpeg to load)
+		original = open_image(path + file)
 
-	# Load gray image
-	gray = open_image('/home/nathan/Pictures/gray.png')
+		if file.split(".")[1] != "png":
+			raise Exception("Sorry, png images only! Please convert your image to png and try again.")
+			sys.exit()
 
-	#Crop and save gray image
-	width, height = gray.size
-	gray.crop((0,0,width-width%7,height-height%7)).save('/home/nathan/Pictures/gray.png','png')
+		# Convert to Grayscale and save
+		new = convert_grayscale(original)
+		save_image(new, path + 'gray.png')
 
-	# Re-load gray image
-	gray = open_image('/home/nathan/Pictures/gray.png')
+		# Load gray image
+		gray = open_image(path + 'gray.png')
 
-	# Convert to dice and save
-	new = convert_dice(gray)
-	save_image(new, '/home/nathan/Pictures/dice.png')
+		# Convert to dice and save
+		new = convert_dice(gray)
+		save_image(new, path + 'dice.png')
+
+	except FileNotFoundError:
+		print("File not found. Please make sure your path and file name are correct and try again.")
+
 
