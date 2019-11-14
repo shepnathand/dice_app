598480ab26b95a733e4e9ce97c1acda014d95081
Nathan <shep.nathan.d@gmail.com>
2019-11-14 12:50:13 -0500

    removed extra code

--- 2dice.py
+++ 2dice.py
@@ -1,45 +1,20 @@
 from PIL import Image
 import sys
 
-# Open an Image
-def open_image(path):
-	newImage = Image.open(path)
-	return newImage
-
-# Save Image
-def save_image(image, path):
-	image.save(path, 'png')
-
-# Create a new image with the given size
-def create_image(i, j):
-	image = Image.new("RGB", (i, j), "white")
-	return image
-
-# Get the pixel from the given image
-def get_pixel(image, i, j):
-	# Inside image bounds?
-	width, height = image.size
-	if i > width or j > height:
-		return None
-
-	# Get Pixel
-	pixel = image.getpixel((i, j))
-	return pixel
-
 # Create a Grayscale version of the image
 def convert_grayscale(image):
 	# Get size
 	width, height = image.size
 
 	# Create new Image and a Pixel Map
-	new = create_image(width, height)
+	new = Image.new("RGB", (width, height), "white")
 	pixels = new.load()
 
 	# Transform to grayscale
 	for i in range(width):
 		for j in range(height):
 			# Get Pixel
-			pixel = get_pixel(image, i, j)
+			pixel = image.getpixel((i,j))
 
 			# Get R, G, B values (This are int from 0 to 255)
 			red = pixel[0]
@@ -62,7 +37,7 @@ def convert_dice(image):
 	width, height = image.size
 
 	# Create new Image and a Pixel Map
-	new = create_image(width*7, height*7)
+	new = Image.new("RGB", (width*7, height*7), "white")
 	pixels = new.load()
 
 	i = 0
@@ -72,7 +47,7 @@ def convert_dice(image):
 
 	while i < height:
 		while j < width:
-			pixel = get_pixel(image,i,j)
+			pixel = image.getpixel((i,j))
 			if pixel[2] > maxSaturation:
 				maxSaturation = pixel[2]
 			count += 1
@@ -87,7 +62,7 @@ def convert_dice(image):
 		while i < width:
 			# Get saturation
 			saturation = 0
-			pixel = get_pixel(image, i, j)
+			pixel = image.getpixel((i,j))
 			saturation += pixel[2]
 
 			for k in range(7):
@@ -129,35 +104,47 @@ def convert_dice(image):
 	# Return new image
 	return new
 
-# Main
-if __name__ == "__main__":
+def main():
+	# process input
+	input = sys.argv[1].strip()
+	delimiter = ""
+
+	if "/" in input:
+		delimiter = "/"
+	elif "\\" in input:
+		delimiter = "\\"
+
+	input = input.split(delimiter)
+	file = input[-1]
+	path = ""
+
+	if len(input) != 1:
+		path = delimiter
+
+	for i in range(len(input)-1):
+		path += input[i] + delimiter
+
 	try:
-		# process input
-		input = sys.argv[1].split("/")
-		file = input[-1]
-		path = "/"
-		for i in range(len(input)-1):
-			path += input[i] + "/"
-
 		# Load Image (JPEG/JPG needs libjpeg to load)
-		original = open_image(path + file)
+		original = Image.open(path + file)
 
-		if file.split(".")[1] != "png":
-			raise Exception("Sorry, png images only! Please convert your image to png and try again.")
-			sys.exit()
+	except FileNotFoundError:
+		print(path + file)
+		print("File not found. Please make sure your path and file name are correct and try again.")
+
+	else:
+		suffix = "." + file.split(".")[-1]
 
 		# Convert to Grayscale and save
 		new = convert_grayscale(original)
-		save_image(new, path + 'gray.png')
+		new.save(path + 'gray' + suffix)
 
 		# Load gray image
-		gray = open_image(path + 'gray.png')
+		gray = Image.open(path + 'gray' + suffix)
 
 		# Convert to dice and save
 		new = convert_dice(gray)
-		save_image(new, path + 'dice.png')
+		new.save(path + 'dice' + suffix)
 
-	except FileNotFoundError:
-		print("File not found. Please make sure your path and file name are correct and try again.")
-
-
+if __name__ == "__main__":
+	main()
