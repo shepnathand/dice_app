3260e8fd18f14fc137fd27e86eefb9ce1825a052
Nathan <shep.nathan.d@gmail.com>
2024-01-14 14:30:09 -0800

    file size

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from django import forms
 
@@ -7,16 +8,14 @@ from django.utils.translation import gettext_lazy as _
 
 class DiceForm(forms.Form):
     image_upload = forms.ImageField(required=False)
-    dice_size = forms.ChoiceField(
-        choices=[('20', '20x20'), ('30', '30x30'), ('40', '40x40')],
-        required=False
-    )
 
     def clean_image_upload(self):
         image_file = self.files.get('image-upload')
         print('Cleaned image file:', image_file)
         if not image_file:
             raise forms.ValidationError('No image file selected.')
+        if image_file.size > 300*1024:
+            raise forms.ValidationError('File too large. Please try again with a file no larger than 300KB.')
         if image_file.file is None:
             raise forms.ValidationError('Error uploading the image file.')
         return image_file
